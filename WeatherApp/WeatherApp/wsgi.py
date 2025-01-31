251689da0724c3fef90287f873893d6a1fd69664
Joyllette <113386253+J-Nyarangi@users.noreply.github.com>
2025-01-31 12:18:48 +0300

    Update wsgi.py

--- WeatherApp/WeatherApp/wsgi.py
+++ WeatherApp/WeatherApp/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for WeatherApp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Update this path to point to your Django project root
+path = '/home/nyarangi/WeatherApp/Weather_App_Django'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Update this to match your project's settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'WeatherApp.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WeatherApp.settings')
-
 application = get_wsgi_application()
